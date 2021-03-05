96f5666c9a6f26f9bd0ece900b1b805839750d4c
Kristen Jestin <48407004+KristenJestin@users.noreply.github.com>
2021-03-05 10:33:13 +0100

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,9 +7,7 @@ const url = '/auth'
 // main
 const register = (credentials) => axios.post(`${url}/register`, credentials)
 const login = (credentials) => axios.post(`${url}/login`, credentials)
-const checkUsername = (username) => axios.post(`${url}/check/username`, username)
-const checkEmail = (email) => axios.post(`${url}/check/email`, email)
 
 // exports
-const service = { login, register, checkUsername, checkEmail }
+const service = { login, register }
 export default service
